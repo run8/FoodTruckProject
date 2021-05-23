ed8126bae738333928ff39ce2271defe7cad984f
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-05-22 19:45:20 -0700

    Completed project minus implementing toString to print

--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.app;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class FoodTruckApp {
@@ -10,19 +11,57 @@ public class FoodTruckApp {
 	public static void main(String[] args) {
 		FoodTruckApp app = new FoodTruckApp();
 		Scanner scanner = new Scanner(System.in);
-		
 		app.run(scanner);
-		app.presentTrucksInfo();
-		System.out.println("Hello");
-//		int selection = app.presentMenuReturnSelection(scanner);
-//		System.out.println("selection: " + selection);
-		
+
+		while (!app.quit) {
+			int selection = app.presentMenuReturnSelection(scanner);
+			switch (selection) {
+			case 1:
+				app.presentTrucksInfo();
+				break;
+			case 2:
+				app.seeAverageRating();
+				break;
+			case 3:
+				app.seeHighestRatedTruck();
+				break;
+			case 4:
+				app.quit = true;
+			default:
+				break;
+			}
+
+		}
 		scanner.close();
 	}
 
-	public void run(Scanner scanner) {
+	public void seeAverageRating() {
+		double sum = 0.0;
+		int numTrucks = 0;
+		for (int i = 0; i < foodTruckArr.length; i++) {
+			if (foodTruckArr[i] != null) {
+				numTrucks += 1;
+				sum += foodTruckArr[i].getRating();
+			}
+		}
+		System.out.println("\nAverage rating of all trucks: " +sum / numTrucks + "\n");
+	}
 
-		while (!quit && ti < 2) {
+	public void seeHighestRatedTruck() {
+		FoodTruck highestRatedTruck = null;
+		int highestRating = 0;
+		for (FoodTruck foodTruck : foodTruckArr) {
+			if (foodTruck != null) {
+				if (foodTruck.getRating() > highestRating) {
+					highestRatedTruck = foodTruck;
+				}
+			}
+		}
+		System.out.println("\n Higheset rated truck is: \n" + highestRatedTruck.toString());
+	}
+
+	public void run(Scanner scanner) {
+		while (!quit && ti < 5) {
 			System.out.print("Enter truck name: ");
 			String name = scanner.next();
 			if (name.equals("quit")) {
@@ -32,51 +71,46 @@ public class FoodTruckApp {
 				String type = scanner.next();
 				System.out.print("Enter rating 0-10: ");
 				int rating = scanner.nextInt();
+				System.out.println("/////////////////////////////////");
 				addTruckToArray(new FoodTruck(name, type, rating));
 				ti++;
 			}
 		}
 	}
 
-//	public String getTruckDataFromUser() {
-//		String info = "";
-//
-//	}
-
 	public void presentTrucksInfo() {
 		for (FoodTruck foodTruck : foodTruckArr) {
-			if(!(foodTruck == null)) {
-			System.out.println("Name: " + foodTruck.getName());
-			System.out.println("Food Type: " + foodTruck.getFoodType());
-			System.out.println("Rating: " + foodTruck.getRating());
-			System.out.println("/////////////////////////////////");
+			if (!(foodTruck == null)) {
+				System.out.println("Name: " + foodTruck.getName());
+				System.out.println("Food Type: " + foodTruck.getFoodType());
+				System.out.println("Rating: " + foodTruck.getRating());
+				System.out.println("/////////////////////////////////");
 			}
 		}
 	}
 
 	public void addTruckToArray(FoodTruck t) {
 		foodTruckArr[ti] = t;
-//		ti++;
 	}
 
 	public int presentMenuReturnSelection(Scanner scanner) {
 		String promptSelection = "Select 1 through 4.\n" + "1. List all existing food trucks.\n"
 				+ "2. See the average rating of food trucks.\n" + "3. Display the highest-rated food truck.\n"
-				+ "4. Quit the program.\n" + "/////////////////////////";
+				+ "4. Quit the program.\n" + "/////////////////////////\n";
 		System.out.println(promptSelection);
 
 		return (scanner.nextInt());
 	}
 
-	public String collectAllExistingFoodTrucks() {
-		String info = "";
-
-		for (int i = 0; i < foodTruckArr.length; i++) {
-			if (!foodTruckArr[i].equals("quit")) {
-				info += foodTruckArr[i].toString();
-			}
-		}
-
-		return info;
-	}
+//	public String listAllExistingFoodTrucks() {
+//		String info = "";
+//
+//		for (int i = 0; i < foodTruckArr.length; i++) {
+//			if (!foodTruckArr[i].equals("quit")) {
+//				info += foodTruckArr[i].toString();
+//			}
+//		}
+//
+//		return info;
+//	}
 }
